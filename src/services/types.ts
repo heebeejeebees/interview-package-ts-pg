03a8094a6779b840b1949a50c86be1d42e2244a7
jaaviieerx <javiertjh@gmail.com>
2023-06-27 23:53:43 +0800

    code cleanup

--- src/services/types.ts
+++ src/services/types.ts
@@ -1,34 +1,6 @@
-import {
-  CreationOptional,
-  InferAttributes,
-  InferCreationAttributes,
-  Model,
-} from 'sequelize';
-
 export type RegisterStudentReq = {
   teacher: string;
   students: string[];
 };
 
 export type StudentStatus = 'Active' | 'Suspended';
-
-export class Student extends Model<
-  InferAttributes<Student>,
-  InferCreationAttributes<Student>
-> {
-  declare id: CreationOptional<number>;
-  declare email: string;
-  declare status: CreationOptional<StudentStatus>;
-  declare createdAt: CreationOptional<string>;
-  declare updatedAt: CreationOptional<string>;
-}
-
-export class Teacher extends Model<
-  InferAttributes<Teacher>,
-  InferCreationAttributes<Teacher>
-> {
-  declare id: CreationOptional<number>;
-  declare email: string;
-  declare createdAt: CreationOptional<string>;
-  declare updatedAt: CreationOptional<string>;
-}

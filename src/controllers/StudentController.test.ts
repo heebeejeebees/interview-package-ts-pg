2df958be31336e1f745b26fd60ea20aaafcd3bda
Javier Teo <javier_teo_from.tp@mom.gov.sg>
2023-07-05 17:59:42 +0800

    draft /register test

---
+++ src/controllers/StudentController.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest';
+import App from '../app';
+import { StudentTeacherRelation, Teacher } from '../config/database';
+// import SequelizeMock from 'sequelize-mock';
+
+/* use sequelize-mock
+https://www.npmjs.com/package/sequelize-mock
+  jest.mock('sequelize', () => {
+  const mSequelize = {
+    authenticate: jest.fn(),
+    define: jest.fn(() => ({ belongsToMany: jest.fn() })),
+  };
+  const actualSequelize = jest.requireActual('sequelize');
+  return {
+    Sequelize: jest.fn(() => mSequelize),
+    DataTypes: actualSequelize.DataTypes,
+  };
+});
+afterAll(() => {
+  jest.resetAllMocks();
+}); */
+
+describe('POST /register', async () => {
+  test('Successful', async () => {
+    const mockTeacher = Teacher.build({ email: 'teacher1@email.com' });
+    jest
+      .spyOn(Teacher, 'findOne')
+      .mockImplementation(() => Promise.resolve(mockTeacher));
+    jest
+      .spyOn(StudentTeacherRelation, 'findOrCreate')
+      .mockImplementation(() => Promise.resolve([mockTeacher, true]));
+    const response = await request(App)
+      .post('/api/register')
+      .send({
+        teacher: 'teacher1@email.com',
+        students: ['student1@email.com', 'student2@email.com'],
+      });
+    expect(response.status).toBe(204);
+  });
+});

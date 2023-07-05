0e3caf411064bcfb05a8f83e6b683be3791f4475
Javier Teo <javier_teo_from.tp@mom.gov.sg>
2023-07-05 16:23:13 +0800

    define routes in router not controllers

--- src/controllers/HealthcheckController.ts
+++ src/controllers/HealthcheckController.ts
@@ -1,12 +1,7 @@
-import Express, { RequestHandler } from 'express';
+import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
-
-// const HealthcheckController = Express.Router();
 
 export const healthcheckHandler: RequestHandler = async (req, res) =>
   res.sendStatus(StatusCodes.OK);
 
-// HealthcheckController.get('/healthcheck', healthcheckHandler);
-
 export default { healthcheckHandler };
-// export default HealthcheckController;
